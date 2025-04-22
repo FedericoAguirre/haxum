40e9f578d462327d0b626711289a1032897607ca
Federico Aguirre <federico.aguirre.cardiel@gmail.com>
2025-04-21 22:42:54 -0600

    Comments updated

--- src/services/tracing.rs
+++ src/services/tracing.rs
@@ -3,8 +3,8 @@ use tracing_subscriber::fmt;
 pub async fn init() {
     // Configure a custom event formatter
     let format = fmt::format()
-        .with_level(true) // don't include levels in formatted output
-        .with_target(true) // don't include targets
+        .with_level(true) // include levels in formatted output
+        .with_target(true) // include targets
         .with_thread_ids(true) // include the thread ID of the current thread
         .with_thread_names(true) // include the name of the current thread
         .with_timer(fmt::time::ChronoLocal::rfc_3339()); // use RFC 3339 format for timestamps
