687a6d68658dd8b71042965e81bad9a5f4252d1b
Federico Aguirre <federico.aguirre.cardiel@gmail.com>
2025-04-27 17:47:35 -0600

    get_pool added

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use axum::Router;
 use haxum::router::router::create_router;
+use haxum::services::redis_service::get_pool;
 use haxum::services::tracing as tracing_service;
 
 #[tokio::main]
 async fn main() {
     tracing_service::init().await;
     tracing::info!("Starting haxum server...");
-
+    tracing::info!("Creating Redis connection pool...");
+    let pool = get_pool().await;
+    tracing::info!("Redis connection pool created");
+    tracing::info!("Creating router...");
     let app: Router = create_router().await;
     tracing::info!("haxum server started on port 3000");
 
--- src/services/redis_service.rs
+++ src/services/redis_service.rs
@@ -1,1 +1,11 @@
-use redis::{Client, RedisError, RedisResult};
+use bb8::{Pool, PooledConnection};
+use bb8_redis::RedisConnectionManager;
+use redis::AsyncCommands;
+
+async fn get_pool() -> Pool<RedisConnectionManager> {
+    tracing::info!("Creating Redis connection pool...");
+    let manager = RedisConnectionManager::new("redis://localhost:6379").unwrap();
+    let pool = bb8::Pool::builder().build(manager).await.unwrap();
+    tracing::info!("Redis connection pool created");
+    pool
+}
