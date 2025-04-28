9c00a7f880b053cceb03e923425c9589b7ece998
Federico Aguirre <federico.aguirre.cardiel@gmail.com>
2025-04-27 18:39:28 -0600

    Redis connection test added

--- src/services/redis_service.rs
+++ src/services/redis_service.rs
@@ -1,10 +1,9 @@
-use bb8::{Pool, PooledConnection};
+use bb8::Pool;
 use bb8_redis::RedisConnectionManager;
-use redis::AsyncCommands;
 
-async fn get_pool() -> Pool<RedisConnectionManager> {
+pub async fn get_pool() -> Pool<RedisConnectionManager> {
     tracing::info!("Creating Redis connection pool...");
-    let manager = RedisConnectionManager::new("redis://localhost:6379").unwrap();
+    let manager = RedisConnectionManager::new("redis://127.0.0.1:6379").unwrap();
     let pool = bb8::Pool::builder().build(manager).await.unwrap();
     tracing::info!("Redis connection pool created");
     pool
