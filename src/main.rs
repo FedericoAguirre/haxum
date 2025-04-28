43de41ba261eec609a9d5521e66f70d1b16bccb3
Federico Aguirre <federico.aguirre.cardiel@gmail.com>
2025-04-27 19:46:10 -0600

    test_redis_connection added

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,18 @@
 use axum::Router;
 use haxum::router::router::create_router;
-use haxum::services::redis_service::get_pool;
+use haxum::services::redis_service::{get_pool, test_redis_connection};
 use haxum::services::tracing as tracing_service;
-use redis::AsyncCommands;
 
 #[tokio::main]
 async fn main() {
     tracing_service::init().await;
     tracing::info!("Starting haxum server...");
+
     tracing::info!("Creating Redis connection pool...");
     let pool = get_pool().await;
-
-    // Redis connection test
-    {
-        // Ping Redis before starting
-        tracing::info!("Creating Redis connection...");
-        let conn_result = pool.get().await;
-
-        if conn_result.is_err() {
-            tracing::error!("Failed to get Redis connection: {:?}", conn_result.err());
-            return;
-        }
-
-        let mut conn = conn_result.unwrap();
-        tracing::info!("Pinging Redis...");
-        let result: String = conn.ping().await.unwrap();
-        assert_eq!(result, "PONG");
-    }
-
+    test_redis_connection(&pool).await;
     tracing::info!("Redis is READY");
+
     tracing::info!("Creating router...");
     let app: Router = create_router().await;
     tracing::info!("haxum server started on port 3000");
--- src/services/redis_service.rs
+++ src/services/redis_service.rs
@@ -1,5 +1,6 @@
 use bb8::Pool;
 use bb8_redis::RedisConnectionManager;
+use redis::AsyncCommands;
 
 pub async fn get_pool() -> Pool<RedisConnectionManager> {
     tracing::info!("Creating Redis connection pool...");
@@ -7,4 +8,30 @@ pub async fn get_pool() -> Pool<RedisConnectionManager> {
     let pool = bb8::Pool::builder().build(manager).await.unwrap();
     tracing::info!("Redis connection pool created");
     pool
+}
+
+pub async fn ping(pool: &Pool<RedisConnectionManager>) -> Result<String, String> {
+    let mut conn = pool.get().await.map_err(|e| e.to_string())?;
+    let result: String = conn.ping().await.map_err(|e| e.to_string())?;
+    if result == "PONG" {
+        Ok("HELLO THERE!".to_string())
+    } else {
+        Err("Failed to ping Redis".to_string())
+    }
+}
+
+pub async fn test_redis_connection(pool: &Pool<RedisConnectionManager>) {
+    // Ping Redis before starting
+    tracing::info!("Creating Redis connection...");
+    let conn_result = pool.get().await;
+
+    if conn_result.is_err() {
+        tracing::error!("Failed to get Redis connection: {:?}", conn_result.err());
+        panic!("Redis is unavailable");
+    }
+
+    let mut conn = conn_result.unwrap();
+    tracing::info!("Pinging Redis...");
+    let result: String = conn.ping().await.unwrap();
+    assert_eq!(result, "PONG");
 }
