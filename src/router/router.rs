9e98fa4b0a942d37cd590b29a771966161bb9076
Federico Aguirre <federico.aguirre.cardiel@gmail.com>
2025-04-29 00:18:06 -0600

    app_state added

--- src/router/router.rs
+++ src/router/router.rs
@@ -1,14 +1,16 @@
+use crate::models::app_state::AppState;
 use axum::Router;
+use axum::extract::State;
 // We import the controllers here so we can use them in the create_router function
 use crate::controllers::{
     hello as hello_controller, ping as ping_controller, string as string_controller,
 };
 
-pub async fn create_router() -> Router {
+pub async fn create_router(app_state: State<AppState>) -> Router {
     // Create a new router and merge all the controllers into it
     Router::new()
         .merge(hello_controller::routes())
-        .merge(ping_controller::routes())
+        .merge(ping_controller::routes(app_state))
         .merge(string_controller::routes())
 }
 
@@ -20,7 +22,11 @@ mod tests {
 
     #[tokio::test]
     async fn test_create_router() {
-        let app = create_router().await;
+        let app_state = AppState {
+            redis_pool: crate::services::redis_service::get_pool().await,
+        };
+        let state = State(app_state.clone());
+        let app = create_router(state.clone()).await;
 
         // Test the hello_controller route
         let response = app
@@ -35,7 +41,7 @@ mod tests {
         assert_eq!(response.status(), StatusCode::OK);
 
         // Test the ping_controller route
-        let app = create_router().await;
+        let app = create_router(state.clone()).await;
 
         let response = app
             .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
@@ -43,7 +49,7 @@ mod tests {
             .unwrap();
         assert_eq!(response.status(), StatusCode::OK);
 
-        let app = create_router().await;
+        let app = create_router(state.clone()).await;
         // Test the string_controller route
         let response = app
             .oneshot(
